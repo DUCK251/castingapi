ace3874624d109c03c18398d6fd8a8eebb99e033
jhlee <leejh8372@gmail.com>
2020-11-16 13:25:01 +0900

    first set up file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+export DATABASE_URL='postgresql:///castingapi'
+export AUTH0_DOMAIN='dev-hrmvva9b.us.auth0.com'
+export ALGORITHMS='RS256'
+export API_AUDIENCE='casting'
+
+# Tokens
+export ASSISTANT='token_value_1'
+export DIRECTOR='token_value_2'
+export PRODUCER='token_value_3'
